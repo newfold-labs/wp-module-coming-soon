c01e4497a6e1d82ddc596a1070be0b783eb3a922
Micah Wood <micah@wpscholar.com>
2024-01-17 16:20:20 -0500

    Change upgrade routine to make sure it runs with the new released version

--- upgrades/1.1.16.php
+++ upgrades/1.1.16.php
@@ -3,15 +3,15 @@
  * Handles update for coming soon module version 1.1.14.
  *
  * Enable coming soon page on fresh installations.
- * 
+ *
  * @package NewfoldLabs\WP\Module\ComingSoon
  */
 
 use NewfoldLabs\WP\Module\ComingSoon\Service;
-use function NewfoldLabs\WP\ModuleLoader\container;
+use NewfoldLabs\WP\ModuleLoader\Container;
 
-add_action( 'newfold_container_set', function () {
-	$isFreshInstall = container()->has( 'isFreshInstallation' ) ? container()->get( 'isFreshInstallation' ) : false;
+add_action( 'newfold_container_set', function ( Container $container ) {
+	$isFreshInstall = $container->has( 'isFreshInstallation' ) ? $container->get( 'isFreshInstallation' ) : false;
 	if ( $isFreshInstall ) {
 		$comingSoonService = new Service();
 		$comingSoonService->enable( false );
