417343b8881b228f855f9d0ccc3296869358b862
Micah Wood <micah@wpscholar.com>
2024-01-24 15:26:13 -0500

    Bump version to 1.1.18

--- bootstrap.php
+++ bootstrap.php
@@ -17,7 +17,7 @@ if ( defined( 'NFD_COMING_SOON_MODULE_VERSION' ) ) {
 	return;
 }
 
-define( 'NFD_COMING_SOON_MODULE_VERSION', '1.1.17' );
+define( 'NFD_COMING_SOON_MODULE_VERSION', '1.1.18' );
 
 require __DIR__ . '/includes/functions.php';
 
