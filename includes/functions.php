86fb0bf993726eeb257aac8e2a602d76dc48be7b
wpalani <abdulrahmanalani95@gmail.com>
2024-11-08 22:25:14 -0700

    Add helper function to check if an option exists

--- includes/functions.php
+++ includes/functions.php
@@ -15,3 +15,13 @@ function isComingSoonActive(): bool {
 function isWoocommerceActive(): bool {
 	return class_exists( 'woocommerce' );
 }
+
+/**
+ * Check if an option exists
+ * 
+ * @param string $option_name The option name
+ */
+function optionExists( $option_name ): bool {
+	$value = get_option( $option_name, 'not_set' );
+	return 'not_set' !== $value;
+}
