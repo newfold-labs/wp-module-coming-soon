d1a80250ee5a2efe83ca979d6977de769885dd44
wpalani <abdulrahmanalani95@gmail.com>
2024-11-11 19:16:36 -0700

    Bump version to 1.3.0

--- bootstrap.php
+++ bootstrap.php
@@ -17,7 +17,7 @@ if ( defined( 'NFD_COMING_SOON_MODULE_VERSION' ) ) {
 	return;
 }
 
-define( 'NFD_COMING_SOON_MODULE_VERSION', '1.2.6' );
+define( 'NFD_COMING_SOON_MODULE_VERSION', '1.3.0' );
 
 require __DIR__ . '/includes/functions.php';
 
