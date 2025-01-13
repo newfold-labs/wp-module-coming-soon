550f04dd045de68ffbbba9e7e19ca6f5e46e630a
Evan Mullins <evanrm@gmail.com>
2025-01-13 17:31:16 -0500

    run `npm run set-version-bump`
    Version updated 1.3.3 => 1.3.4

--- bootstrap.php
+++ bootstrap.php
@@ -17,7 +17,7 @@ if ( defined( 'NFD_COMING_SOON_MODULE_VERSION' ) ) {
 	return;
 }
 
-define( 'NFD_COMING_SOON_MODULE_VERSION', '1.3.3' );
+define( 'NFD_COMING_SOON_MODULE_VERSION', '1.3.4' );
 
 require __DIR__ . '/includes/functions.php';
 
---
+++ build/1.3.4/coming-soon.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2881219076ba99d271c3');
