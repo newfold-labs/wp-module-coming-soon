2ff917e7b4ef46e9ba82a5b86d14af07765dcbc4
Evan Mullins <evanrm@gmail.com>
2025-02-05 15:14:12 -0500

    set-version-bump Version updated 1.3.5 => 1.3.6

--- bootstrap.php
+++ bootstrap.php
@@ -17,7 +17,7 @@ if ( defined( 'NFD_COMING_SOON_MODULE_VERSION' ) ) {
 	return;
 }
 
-define( 'NFD_COMING_SOON_MODULE_VERSION', '1.3.5' );
+define( 'NFD_COMING_SOON_MODULE_VERSION', '1.3.6' );
 
 require __DIR__ . '/includes/functions.php';
 
