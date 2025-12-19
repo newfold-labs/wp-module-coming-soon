627ca34b00f7c526a20426419ef47b9cda049553
Evan Mullins <evanrm@gmail.com>
2025-12-19 14:34:37 -0500

    remove skipped outdated tests

--- tests/playwright/specs/coming-soon.spec.mjs
+++ tests/playwright/specs/coming-soon.spec.mjs
@@ -51,24 +51,6 @@ test.describe('Coming Soon', () => {
     await verifyComingSoonActive(page, appClass);
   });
 
-  test.skip('Has Coming Soon Section on Home', async ({ page }) => {
-    // This test is skipped because the portal has been removed from the home page.
-    // We can remove this test if we dont end up bringing the portal back.
-    await navigateToHome(page, pluginId);
-
-    const homeSection = page.locator(`${appClass}-home .nfd-app-section-content`);
-    await expect(homeSection.locator('a.nfd-button').filter({ hasText: 'View' }).first()).toHaveCount(1);
-    await expect(homeSection.locator('a.nfd-button').filter({ hasText: 'Edit' }).first()).toHaveCount(1);
-    await expect(homeSection.first().locator('button').filter({ hasText: 'Launch' })).toHaveCount(1);
-
-    // Coming Soon Admin bar links to setting
-    const adminBarBadge = getAdminBarBadge(page);
-    await adminBarBadge.locator('a.ab-item').click();
-    
-    const url = page.url();
-    expect(url).toContain('#/settings');
-  });
-
   test('Coming Soon Toggle Turns Coming Soon Off', async ({ page }) => {
     await navigateToSettings(page, pluginId);
     
@@ -110,30 +92,4 @@ test.describe('Coming Soon', () => {
     await verifyComingSoonFrontend(page);
   });
 
-  test.skip('Launching launches site', async ({ page }) => {
-    // This test is already in the ecommerce module, and the code is in the ecommerce module
-    // once the component is moved into this module this test will be used and the ecom test removed
-    await navigateToSettings(page, pluginId);
-    
-    const toggle = getComingSoonToggle(page);
-    await expect(toggle).toHaveAttribute('aria-checked', 'true');
-
-    await navigateToHome(page, pluginId);
-
-    const homeSection = page.locator(`${appClass}-home .nfd-app-section-content`);
-    await homeSection.first().locator('button').filter({ hasText: 'Launch your' }).click();
-    await page.waitForTimeout(100);
-
-    await expect(homeSection.first().locator('button').filter({ hasText: 'Launch your' })).toHaveCount(0);
-
-    await auth.logout(page);
-    await navigateToFrontend(page);
-    await verifySiteLiveFrontend(page);
-
-    await auth.loginToWordPress(page);
-    await navigateToSettings(page, pluginId);
-
-    const toggleAfter = getComingSoonToggle(page);
-    await expect(toggleAfter).toHaveAttribute('aria-checked', 'false');
-  });
 });
